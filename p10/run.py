8743606891ddf6578c9539de5bd9de7ac2fa28ae
Avraam Tsantekidis <makistsantekidis@gmail.com>
2019-12-12 11:04:04 +0000

    golf d10p1

--- p10/run.py
+++ p10/run.py
@@ -3,47 +3,16 @@ import numpy as np
 if __name__ == '__main__':
     matrix = np.asarray([list(l) for l in open('inputs.txt').read().splitlines()])
     locations = np.argwhere(matrix == '#')
-    # obs_list = []
-    # for i, cand in enumerate(locations):
-    #     lcopy = np.delete(locations.copy(), i, axis=0)
-    #     diffs = cand - lcopy
-    #     nb_observables = 0
-    #     while len(diffs) > 0:
-    #         for i in range(len(diffs)):
-    #             cdiff = diffs[i]
-    #             cdiff = (cdiff // abs(np.gcd(cdiff[0], cdiff[1])))
-    #             shadow_idxs = ((diffs % cdiff) == 0) & (np.sign(cdiff) == np.sign(diffs))
-    #             frac = (diffs // cdiff)
-    #             shadow_idxs = shadow_idxs.all(axis=1) & ((frac[:, 0] == frac[:, 1]) |  ((frac == 0).any(axis=1)))
-    #             nb_observables += 1
-    #             diffs = np.delete(diffs, np.argwhere(shadow_idxs).flatten(), axis=0)
-    #             if i + 1 >= len(diffs):
-    #                 break
-    #     obs_list.append(nb_observables)
+    diff_matrix = locations[..., np.newaxis] - locations.T
+    diff_matrix //= np.gcd(*diff_matrix.transpose(1, 0, 2))[:, np.newaxis]
+    print(max([len(np.unique(diffs.T, axis=0)) for diffs in diff_matrix]) - 1)
 
-    # m2 = np.argwhere(np.ones(matrix.shape))
-    # m2 = m2 - 16
-    # rads = np.arctan2(m2[:, 1], m2[:, 0]).reshape(matrix.shape)
-
-    # for i, cand in enumerate(locations):
-    #     lcopy = np.delete(locations.copy(), i, axis=0)
-    #     diffs = cand - lcopy
-    #     fracz = diffs[:, 1] / diffs[:, 0]
-    #     sdiffs = np.sign(diffs)
-    #     nb_observables = 0
-    #     while len(diffs) > 0:
-    #         shadow_idxs = (fracz == fracz[0]) & ((sdiffs[0] == sdiffs).all(axis=1))
-    #         diffs = diffs[~shadow_idxs]
-    #         fracz = fracz[~shadow_idxs]
-    #         nb_observables += 1
-    #     obs_list.append(nb_observables)
-    # print(max(obs_list))
-    observable_counts = []
-    for i, candidate in enumerate(locations):
-        diffs = candidate - locations
-        diffs //= np.gcd(*diffs.T)[:, np.newaxis]
-        observable_counts.append(len(np.unique(diffs, axis=0)))
-    print(max(observable_counts) - 1)
+    # observable_counts = []
+    # for i, candidate in enumerate(locations):
+    #     diffs = candidate - locations
+    #     diffs //= np.gcd(*diffs.T)[:, np.newaxis]
+    #     observable_counts.append(len(np.unique(diffs, axis=0)))
+    # print(max(observable_counts) - 1)
 
     Nth = 200
     station_coords = locations[np.argmax(observable_counts)]
